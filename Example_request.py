a353b8da4676dc1665b0578fa84eefd9ed32cdf4
zeeshan <skzeeshan3650@gmail.com>
2023-07-08 00:07:55 +0530

    test

--- Example_request.py
+++
@@ -1,8 +0,0 @@
-import vocalhost
-
-vocalhost.API_KEY = 'ff90d1d4-f9ef-4c15-a987-fcdea3a9d13c'
-
-while True:
-    message = input('Send a message: ')
-    response = vocalhost.Request.send(message, receiver_id='3')
-    print('received: ' + response.text)
