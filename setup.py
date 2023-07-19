2bbc65ba2a6a3f1f3734dc0a2d9bbe77f653105d
zeeshan <skzeeshan3650@gmail.com>
2023-07-19 21:13:31 +0530

    1.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='vocalhost',
-    version='1.1.5',
+    version='1.1.6',
     py_modules=['vocalhost'],
     install_requires=[
         'websockets',
